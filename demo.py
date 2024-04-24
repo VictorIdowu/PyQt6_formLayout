62189e8e7b71caedb5682fb35a57aa1f0364edb5
VictorIdowu <idsonvictor@gmail.com>
2024-04-24 16:59:19 +0100

    Qstacked Layout

--- demo.py
+++ demo.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QWidget,QApplication,QFormLayout,QLineEdit,QLabel,QComboBox,QTextEdit,QPushButton
+from PyQt6.QtWidgets import QWidget,QApplication,QVBoxLayout,QStackedLayout,QFormLayout,QLineEdit,QLabel,QComboBox,QTextEdit,QPushButton
 
 class Window(QWidget):
   def __init__(self) :
@@ -12,8 +12,17 @@ class Window(QWidget):
     self.setWindowTitle("Form Layout")
     self.setGeometry(100,100,400,200)
 
+    self.combo_box = QComboBox()
+    self.combo_box.addItems(["Label", "Form"])
+    self.combo_box.activated.connect(self.switch_pages)
+
+    # Creating Page 1
+    label = QLabel("This is a label page")
+
+    # Creating Page 2
     form_layout = QFormLayout()
-    self.setLayout(form_layout)
+    page2_containter = QWidget()
+    page2_containter.setLayout(form_layout)
 
     # Labels and inputs
     self.name_input = QLineEdit()
@@ -37,7 +46,19 @@ class Window(QWidget):
     form_layout.addRow(QLabel("Message"),self.message_input)
     form_layout.addRow(submit_button)
 
+    # Creating a stacked layout
+    self.stacked_layout = QStackedLayout()
+    self.stacked_layout.addWidget(label)
+    self.stacked_layout.addWidget(page2_containter)
 
+    main_layout = QVBoxLayout()
+    main_layout.addWidget(self.combo_box)
+    main_layout.addLayout(self.stacked_layout)
+    self.setLayout(main_layout)
+
+  def switch_pages(self, index):
+    self.stacked_layout.setCurrentIndex(index)
+    
   def submit_clicked(self):
     name = self.name_input.text()
     email = self.email_input.text()
