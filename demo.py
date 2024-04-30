41e3fbfd354165c914c73d0aa4c54b5ddc3b4598
VictorIdowu <idsonvictor@gmail.com>
2024-04-30 14:15:58 +0100

    toolbar

--- demo.py
+++ demo.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QMainWindow,QWidget,QApplication,QVBoxLayout,QStackedLayout,QFormLayout,QLineEdit,QLabel,QComboBox,QTextEdit,QPushButton
-from PyQt6.QtGui import QAction
+from PyQt6.QtGui import QAction,QIcon
 
 class Window(QMainWindow):
   def __init__(self) :
@@ -13,40 +13,23 @@ class Window(QMainWindow):
     self.setWindowTitle("Menu")
     self.setGeometry(100,100,400,300)
 
-    # Step 1 Create a menubar
-    menubar = self.menuBar()
+    toolbar = self.addToolBar("Main Toolbar")
 
-    # Creating menu items
-    file_menu = menubar.addMenu("File")
+    self.new_action = QAction(QIcon("icons/new_file.png"),"New")
+    toolbar.addAction(self.new_action)
 
-    # Creating an action
-    self.new_action = QAction("New")
-    # Adding action to the menu
-    file_menu.addAction(self.new_action)
+    toolbar.addSeparator()
 
-    # Adding a seperator
-    file_menu.addSeparator()
+    self.open_action = QAction(QIcon("icons/open.png"),"Open")
+    toolbar.addAction(self.open_action)
 
-    # Creating exit action
-    self.exit_action = QAction("Exit")
-    # Adding action to the menu
-    file_menu.addAction(self.exit_action)
-    
-    # Creating edit menu
-    edit_menu = menubar.addMenu("Edit")
+    toolbar.addSeparator()
 
+    self.save_action = QAction(QIcon("icons/save.png"),"Save")
+    toolbar.addAction(self.save_action)
     
 
-    self.copy_action = QAction("Copy")
-    edit_menu.addAction(self.copy_action)
-    edit_menu.addSeparator()
     
-    self.cut_action = QAction("Cut")
-    edit_menu.addAction(self.cut_action)
-    edit_menu.addSeparator()
-    
-    self.paste_action = QAction("Paste")
-    edit_menu.addAction(self.paste_action)
      
 
     
